030896f532556e7db35743cbbc3fbd75a4b873bb
YULIANA ROLDAN CARVAJAL <yroldancarvajal@gmail.com>
2021-08-26 20:53:20 -0500

    Creacion del ambinete hasta la base de datos

---
+++ Vehiculos/Vehiculos/Data/DataContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Vehiculos.Data.Entities;
+
+namespace Vehiculos.Data
+{
+    //Class where is create of connection with the DB
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<VehiculeType> VehiculeTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<VehiculeType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
---
+++ Vehiculos/Vehiculos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Vehiculos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
